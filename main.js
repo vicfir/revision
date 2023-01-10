25f58109129dce0f5ade7ae405feb992c496bbaa
vicfir <vicfir@hotmail.fr>
2023-01-10 14:01:49 +0100

    exo array 2

--- main.js
+++ main.js
@@ -95,3 +95,6 @@
 //Array---------------------------------------------------------
 let arr1 = [];
 console.log(arr1, typeof arr1);
+
+let arr2 = ["txt", 34, true, []];
+console.log(arr2[0],arr2[1], arr2[2], arr2[3]);
